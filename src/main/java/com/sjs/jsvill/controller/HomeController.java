7c204fa95a491bc580eaf7c3d42579c818cfb9ee
Sean <ksw6125000@naver.com>
2021-05-19 00:00:24 +0900

    ..

--- src/main/java/com/sjs/jsvill/controller/HomeController.java
+++ src/main/java/com/sjs/jsvill/controller/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 //메인 컨트롤러2
+//메인 컨트롤러2
     @GetMapping("/")
     public String home() {
         return "index";
