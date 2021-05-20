3945578ee7c2449ba917e6177a14be4ecbabadbd
Sean <ksw6125000@naver.com>
2021-05-20 23:42:00 +0900

    ..

--- src/main/java/com/sjs/jsvill/controller/HomeController.java
+++ src/main/java/com/sjs/jsvill/controller/HomeController.java
@@ -6,13 +6,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 //메인 컨트롤러
+//메인 컨트롤러2
     @GetMapping("/")
     public String home() {
-        return "main";
+        return "main/main";
+    }
+
+    @GetMapping("editBuilding")
+    public String editBuilding() {
+        return "buildingManagement/editBuilding";
+    }
+
+    @GetMapping("registerBuilding")
+    public String registerBuilding() {
+        return "buildingManagement/registerBuilding";
     }
 
     @GetMapping("registerUnit")
     public String registerUnit() {
-        return "registerUnit";
+        return "buildingManagement/registerUnit";
+    }
+
+    @GetMapping("community")
+    public String community() {
+        return "community/community";
+    }
+
+    @GetMapping("communityWrite")
+    public String communityWrite() {
+        return "community/communityWrite";
     }
 }
