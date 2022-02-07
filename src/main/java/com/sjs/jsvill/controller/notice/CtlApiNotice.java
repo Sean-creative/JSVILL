2f83892e0958f9216070b305b1d27209a255e0c2
ohmjeemin <ohmjeemin@gmail.com>
2022-02-07 22:48:58 +0900

    컨트롤러 - html 연결

---
+++ src/main/java/com/sjs/jsvill/controller/notice/CtlApiNotice.java
@@ -0,0 +1,20 @@
+package com.sjs.jsvill.controller.notice;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Log4j2
+@RequiredArgsConstructor
+public class CtlApiNotice {
+
+    @RequestMapping("/notice")
+    public String appHome() {
+        log.info("user notice");
+        return "/app/notice";
+    }
+
+
+}
---
+++ src/main/java/com/sjs/jsvill/controller/notice/CtlApiNoticeV.java
@@ -0,0 +1,20 @@
+package com.sjs.jsvill.controller.notice;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Log4j2
+@RequiredArgsConstructor
+public class CtlApiNoticeV {
+
+    @RequestMapping("/notice/v")
+    public String appHome() {
+        log.info("user noticeDetail");
+        return "/app/noticeDetail";
+    }
+
+
+}
