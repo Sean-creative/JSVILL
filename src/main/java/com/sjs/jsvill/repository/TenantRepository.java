8134a7ac395346fd8cf3783c6f9ede1ea17e506a
ohmjeemin <ohmjeemin@gmail.com>
2022-04-05 23:56:50 +0900

    tenant-findByPhone추가

--- src/main/java/com/sjs/jsvill/repository/TenantRepository.java
+++ src/main/java/com/sjs/jsvill/repository/TenantRepository.java
@@ -11,4 +11,6 @@ public interface TenantRepository extends JpaRepository<Tenant, Long> {
 
     @Query("select t from ContractTenant ct inner join ct.tenant t where ct.contract.contract_rowid=:contract_rowid")
     List<Tenant> findByContract(@Param("contract_rowid") Long contract_rowid);
+
+    Tenant findByPhone(String phone);
 }
