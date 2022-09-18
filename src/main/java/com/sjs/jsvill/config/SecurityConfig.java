640fc0c22dfc4212fa6a9c98f77f99e3e14b196a
seonwoo <ksw6125000@naver.com>
2022-09-18 17:57:07 +0900

    로그아웃 구현 성공

--- src/main/java/com/sjs/jsvill/config/SecurityConfig.java
+++ src/main/java/com/sjs/jsvill/config/SecurityConfig.java
@@ -41,10 +41,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //        http.rememberMe().tokenValiditySeconds(60 * 60 * 24 * 3).userDetailsService(userDetailsService); //3days
         http.csrf().disable();  //음.. 나중에 적용하면 좋나?
         http.authorizeRequests()
-//                .antMatchers("/home").permitAll()
-                .antMatchers("/sample/member").hasRole("USER")
-                .antMatchers("/home").hasRole("USER")
-                .anyRequest().authenticated();
+                .antMatchers("/member/login").permitAll()
+                .antMatchers("/login").permitAll()
+                .anyRequest().authenticated(); //anyRequest는 antMatchers로 지정한 url 이외의 모든 url을 지정하는 메소드, else같은 느낌임
 
 
         //인가/인증에 문제시 로그인 화면
@@ -52,14 +51,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //        loginProcessingUrl() : 로그인 정보를 보낼 액션 페이지
 //        defaultSuccessUrl() : 로그인 성공 시 보낼 페이지
         http.formLogin()
-                .loginPage("/member/login") //이걸로 별도의 로그인 페이지 이용
+                .loginPage("/login") //이걸로 별도의 로그인 페이지 이용
                 .usernameParameter("phoneNumber")
                 .passwordParameter("pinNumber")
-                .loginProcessingUrl("/login_proc")
-                .defaultSuccessUrl("/home")
-                .permitAll();
+                .loginProcessingUrl("/doLogin")
+                .defaultSuccessUrl("/home");
+//                .successHandler(new MyLoginSuccessHandler());
 
         http.logout() //로그아웃 했을 때 지정해놓은 페이지 볼 수 있음
-                .logoutUrl("/member/login");
+                .logoutUrl("/doLogout")
+                .logoutSuccessUrl("/login");
     }
 }
