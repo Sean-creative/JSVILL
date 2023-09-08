cf0641b29db3ab138c5a5d0a4fcac79832c9902f
seonwoo <ksw6125000@naver.com>
2023-09-08 15:15:59 +0900

    ..

--- src/main/java/com/sjs/jsvill/repository/MemoRepository.java
+++ src/main/java/com/sjs/jsvill/repository/MemoRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
-    List<Memo> findByUnitOrderByMemo_rowidDesc(Unit unit);
+    List<Memo> findByUnit(Unit unit);
 }
