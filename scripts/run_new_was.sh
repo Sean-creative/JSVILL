ca630e64a8cb518aa00689ba74f48c2e8699f4b1
seonwoo <ksw6125000@naver.com>
2023-11-09 17:36:16 +0900

    무중단 배포 구현 ..1 #9

---
+++ scripts/run_new_was.sh
@@ -0,0 +1,48 @@
+# run_new_was.sh
+
+#!/bin/bash
+
+PROJECT_ROOT="/home/ubuntu/spring-github-action" # 프로젝트 루트
+JAR="$ROOT_PATH/application.jar"
+
+APP_LOG="$ROOT_PATH/application.log"
+ERROR_LOG="$ROOT_PATH/error.log"
+START_LOG="$ROOT_PATH/start.log"
+
+# service_url.inc 에서 현재 서비스를 하고 있는 WAS의 포트 번호 가져오기
+CURRENT_PORT=$(cat /home/ec2-user/service_url.inc | grep -Po '[0-9]+' | tail -1)
+TARGET_PORT=0
+
+echo "> Current port of running WAS is ${CURRENT_PORT}."
+
+if [ ${CURRENT_PORT} -eq 8081 ]; then
+  TARGET_PORT=8082 # 현재포트가 8081이면 8082로 배포
+elif [ ${CURRENT_PORT} -eq 8082 ]; then
+  TARGET_PORT=8081 # 현재포트가 8082라면 8081로 배포
+else
+  echo "> Not connected to nginx" # nginx가 실행되고 있지 않다면 에러 코드
+fi
+
+# 현재 포트의 PID를 불러온다
+TARGET_PID=$(lsof -Fp -i TCP:${TARGET_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
+
+# PID를 이용해 해당 포트 서버 Kill
+if [ ! -z ${TARGET_PID} ]; then
+  echo "> Kill ${TARGET_PORT}."
+  sudo kill ${TARGET_PID}
+fi
+
+echo "[$NOW] $JAR 복사" >> $START_LOG
+cp $ROOT_PATH/build/libs/*.jar $JAR
+
+iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
+iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 81 -j REDIRECT --to-port 8081
+
+# 타켓 포트에 jar파일을 이용해 새로운 서버 실행
+echo "[$NOW] > $JAR 실행 $TARGET_PORT Port" >> $START_LOG
+nohup java -jar -Dserver.port=${TARGET_PORT} ${JAR_FILE} > $APP_LOG 2> $ERROR_LOG &
+
+SERVICE_PID=$(pgrep -f $JAR)
+echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
+
+exit 0
