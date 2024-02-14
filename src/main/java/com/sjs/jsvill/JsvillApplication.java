b806b327b513bc5460d0547af0374d317ab3483e
Sean-creative <ksw6125000@naver.com>
2024-02-15 03:12:55 +0900

    tailwinds.css 도입4 - 스프링 부트의 실행환경을 조절해서 task 처리 완료

--- src/main/java/com/sjs/jsvill/JsvillApplication.java
+++ src/main/java/com/sjs/jsvill/JsvillApplication.java
@@ -17,6 +17,7 @@ public class JsvillApplication {
 		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
 	}
 	public static void main(String[] args) {
+
 		SpringApplication.run(JsvillApplication.class, args);
 	}
 }
